35a777b7dc8ce8a7fb7dd9403b0815852e28b7d8
Nick Persad <nick_persad@outlook.com>
2020-10-28 23:15:30 -0400

    Super simple monaco editor in Vue.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <CanvasEditor class="editor" v-model="code" language="javascript" />
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CanvasEditor from './components/CanvasEditor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CanvasEditor
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, #app {
+	position: absolute;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
 }
 </style>
---
+++ src/components/CanvasEditor.vue
@@ -0,0 +1,30 @@
+<template>
+  <CanvasEditor class="editor" v-model="code" language="javascript" theme="vs-dark" automaticLayout="true" />
+</template>
+
+<script>
+import CanvasEditor from 'vue-monaco'
+
+export default {
+  components: {
+    CanvasEditor
+  },
+
+  data() {
+    return {
+      code: `const hello = () => {
+    return "world";
+}`
+    }
+  }
+}
+</script>
+
+<style>
+.editor {
+  width: 100%;
+  min-width: 800px;
+  /* height: 800px; */
+  height: 100vh;
+}
+</style>
